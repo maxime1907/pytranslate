4bc42b049244b65d6507eaa1ac8023c8693159b6
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2025-01-05 22:45:39 +0100

    fix(deepl): character limit of 1500 words, py312 upgrade, github ci

--- src/pytranslate/cli.py
+++ src/pytranslate/cli.py
@@ -12,12 +12,44 @@ AVAILABLE_APIS = ["google", "deepl"]
 
 
 @click.command()
-@click.option("--log-level", help="Logging level", default="INFO", type=click.Choice(list(logging._nameToLevel.keys())))
-@click.option("--log-format", help="Logging format", default="%(asctime)s | %(levelname)s | %(message)s", type=str)
-@click.option("--log-dateformat", help="Logging date format", default="%Y-%m-%dT%H:%M:%S%z", type=str)
-@click.option("--source", "-s", help="Source language", required=True, type=click.Choice(AVAILABLE_LANGUAGES))
-@click.option("--destination", "-d", help="Destination language", required=True, type=click.Choice(AVAILABLE_LANGUAGES))
-@click.option("--api", help="Translation API to use", required=True, type=click.Choice(AVAILABLE_APIS))
+@click.option(
+    "--log-level",
+    help="Logging level",
+    default="INFO",
+    type=click.Choice(list(logging._nameToLevel.keys())),
+)
+@click.option(
+    "--log-format",
+    help="Logging format",
+    default="%(asctime)s | %(levelname)s | %(message)s",
+    type=str,
+)
+@click.option(
+    "--log-dateformat",
+    help="Logging date format",
+    default="%Y-%m-%dT%H:%M:%S%z",
+    type=str,
+)
+@click.option(
+    "--source",
+    "-s",
+    help="Source language",
+    required=True,
+    type=click.Choice(AVAILABLE_LANGUAGES),
+)
+@click.option(
+    "--destination",
+    "-d",
+    help="Destination language",
+    required=True,
+    type=click.Choice(AVAILABLE_LANGUAGES),
+)
+@click.option(
+    "--api",
+    help="Translation API to use",
+    required=True,
+    type=click.Choice(AVAILABLE_APIS),
+)
 @click.option("--input", help="File to translate", required=True, type=str)
 def cli(
     log_level: str,
@@ -28,7 +60,6 @@ def cli(
     api: str,
     input: str,
 ) -> None:
-
     logging.basicConfig(
         level=log_level,
         format=log_format,
@@ -37,7 +68,9 @@ def cli(
 
     file_extension = pathlib.Path(input).suffix
     if file_extension == ".ass":
-        translator = ASSTranslator(file=input, tolang=destination, fromlang=source, api=api)
+        translator = ASSTranslator(
+            file=input, tolang=destination, fromlang=source, api=api
+        )
         translator.run()
     else:
         raise Exception("File type not supported")
